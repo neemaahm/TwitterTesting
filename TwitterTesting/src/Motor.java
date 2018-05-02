cbad9b528763c7191d731fae7e8abccdcd7b5a9b
Profious <neemaahm@gmail.com>
2018-05-02 10:52:30 -0400

    Added pwm method and a second on() method

--- TwitterTesting/src/Motor.java
+++ TwitterTesting/src/Motor.java
@@ -12,22 +12,44 @@ public class Motor {
         status = false;
     }
 
-    //turns the motor on
-    public void on(int milliseconds){
-        status = true;
-        System.out.println(name + ": on");
-        try {
-                Thread.sleep(milliseconds); //sleep amount of milliseconds
-                off();
-            } catch (InterruptedException e) {
-                System.out.println("got interrupted!");
-    }
-
     //turns the motor off
-    public void off(){
+    public void off() {
         status = false;
-        System.out.println(name + ": off");
+        System.out.println(/*name + */": off");
     }
 
+    public void on() {
+        status = true;
+        System.out.println(/*name + */": on");
+    }
 
+    //turns the motor on for a certain amount of time
+    public void on(int milliseconds) {
+        on();
+        try {
+            Thread.sleep(milliseconds); //sleep amount of milliseconds
+            off();
+        } catch (InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+    }
+
+    public void pwm(double dutyCycle, int duration){
+        int ratio = ((int)((dutyCycle+0.05)*10));
+        int resolution = 10;
+        System.out.println(ratio);
+        int numbOfLoops = duration*(1000/resolution);
+        for(int i=0; i<numbOfLoops; i++){
+            if (i<ratio){
+                System.out.println("ONIT");
+                on();
+                try{ Thread.sleep(resolution);} catch(InterruptedException e){ System.out.println("Interrupted");}
+            } else {
+                System.out.println("OFFIT");
+                off();
+                try{ Thread.sleep(resolution);} catch(InterruptedException e){ System.out.println("Interrupted");}
+            }
+        }
+
+    }
 }
